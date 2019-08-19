695449833cac95c593e2696eecd3be25e6e2e7ba
Stefanos <sagelastos@gmail.com>
2019-08-19 10:47:57 +0200

    Header Tabs linked to router changes. all ok

--- src/MyIconTabs.tsx
+++ src/MyIconTabs.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Router from 'next/router';
 
 const useStyles = makeStyles({
   root: {
@@ -12,11 +13,30 @@ const useStyles = makeStyles({
 
 export default function CenteredTabs() {
   const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState("/");
 
-  function handleChange(_event: React.ChangeEvent<{}>, newValue: number) {
+  function handleChange(_event: React.ChangeEvent<{}>, newValue: string) {
+    Router.push(newValue);
     setValue(newValue);
   }
+
+  useEffect(() => {
+    // Set initial state of the Tabs component for SSR
+    Router.pathname == "/"
+      ? setValue("/")
+      : Router.pathname == "/contact"
+        ? setValue("/contact")
+        : setValue("/projects");
+
+    Router.events.on("routeChangeStart", url => {
+      // Change the state of the Tabs depending on URL change
+      url == "/"
+        ? setValue("/")
+        : url == "/contact"
+          ? setValue("/contact")
+          : setValue("/projects");
+    });
+  }, []); // run an effect and clean it up only once (on mount and unmount)
 
   return (
     <Paper className={classes.root}>
@@ -27,9 +47,9 @@ export default function CenteredTabs() {
         textColor="primary"
         centered
       >
-        <Tab label="Home" />
-        <Tab label="My projects" />
-        <Tab label="Contact" />
+        <Tab label="Home" value="/" />
+        <Tab label="My projects" value="/projects" />
+        <Tab label="Contact" value="/contact" />
       </Tabs>
     </Paper>
   );
