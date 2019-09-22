e76c5a5e1798ba4ae923c9fe7ba79492bc173c4e
stefanos <sagelastos@gmail.com>
2019-09-22 22:52:04 +0200

    chore: regen site

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "16a46457b09dc0e66bc7";
+/******/ 	var hotCurrentHash = "06a113553ae1fdb3c770";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
