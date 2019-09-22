dfd1dab7b6e89a9694278d1e46de9b69b64c713a
stefanos <sagelastos@gmail.com>
2019-09-22 18:07:07 +0200

    added novoresume projects, updated angular and Jav

--- pages/projects/[pid].tsx
+++ pages/projects/[pid].tsx
@@ -22,18 +22,14 @@ class ProjectPage extends React.Component<Props> {
     const markdown = await getREADMEfromMyGithub(query.pid as string);
     const imageUrl = getProjectImageUrl(query.pid as string);
     var snack = true;
-    if(query.pid == "ar-tour-guide" || query.pid == "chat-webapp-node-js" || query.pid == "next-js-homepage"){
-      snack = false;
-    }
+    snack = false;
     return { markdown, imageUrl, snack };
   }
 
   render() {
     return (
       <React.Fragment>
-        { this.props.snack &&
-          <SimpleSnackbar />
-          }
+        {this.props.snack && <SimpleSnackbar />}
         <MarkdownLayout
           projectMD={this.props.markdown}
           backroundImage={this.props.imageUrl}
